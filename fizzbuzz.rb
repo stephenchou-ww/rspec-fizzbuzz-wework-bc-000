7ac2a3e4663a7f4add95d9b92cdf98e46a63a1b8
stephenchou-ww <stephen.chou@wework.com>
2018-02-19 20:23:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,12 @@ def fizzbuzz(x)
   
   if x % 3 == 0
     return "Fizz"
-  end
   
   elsif x % 5 == 0
     return "Buzz"
-  end
   
-  if x % 15 == 0
+  elsif x % 15 == 0
     return "FizzBuzz"
-  end
   
   else
     return nil
