d1ca2097abdb7681a708075010d9932e5848a994
stephenchou-ww <stephen.chou@wework.com>
2018-02-19 20:28:27 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+
+def fizzbuzz(x)
+  
+  if x % 3 == 0 && x % 15 != 0
+    return "Fizz"
+  
+  elsif x % 5 == 0 && x % 15 != 0
+    return "Buzz"
+  
+  elsif x % 15 == 0
+    return "FizzBuzz"
+  
+  else
+    return nil
+  end
+  
+end
+
